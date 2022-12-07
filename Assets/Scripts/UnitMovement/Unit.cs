0e9a4b1a04da12fff8414d647830f61cea191e7a
Mykola Kalchuk <80714127+NickKRay1234@users.noreply.github.com>
2022-12-07 16:44:51 +0100

    Move Unit;

--- Assets/Scripts/UnitMovement/Unit.cs
+++ Assets/Scripts/UnitMovement/Unit.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
     private Vector3 _targetPosition;
     private Vector3 _moveDirection;
+    private const  float StoppingDistance = 0.1f;
     private const float Speed = 4.0f;
     
 
     private void Update()
     {
-        _moveDirection = (_targetPosition - transform.position).normalized;
-        transform.position += _moveDirection * (Speed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, _targetPosition) > StoppingDistance)
+        {
+            _moveDirection = (_targetPosition - transform.position).normalized;
+            transform.position += _moveDirection * (Speed * Time.deltaTime);
+        }
 
         if (Input.GetKeyDown(KeyCode.T))
         {
