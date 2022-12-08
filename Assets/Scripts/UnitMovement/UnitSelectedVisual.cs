7048baf7235f61dc822a9852a4620bbeeced4866
Mykola Kalchuk <80714127+NickKRay1234@users.noreply.github.com>
2022-12-08 18:44:09 +0100

    Added UnitSelectedVisual.cs;

---
+++ Assets/Scripts/UnitMovement/UnitSelectedVisual.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+public class UnitSelectedVisual : MonoBehaviour
+{
+    [SerializeField] private Unit _unit;
+    private MeshRenderer _meshRenderer;
+
+    private void Awake()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
+}
