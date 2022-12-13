8e517c34e61235f19cbb3c9ede4a1c56cc63e52c
Mykola Kalchuk <80714127+NickKRay1234@users.noreply.github.com>
2022-12-13 15:46:41 +0100

    Added camera logic;

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using Cinemachine;
+public class CameraController : MonoBehaviour
+    {
+        private const float MIN_FOLLOW_Y_OFFSET = 2f;
+        private const float MAX_FOLLOW_Y_OFFSET = 12f;
+        [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+        private void Update()
+        {
+            Vector3 inputMoveDirection = new Vector3(0, 0, 0);
+            if (Input.GetKey(KeyCode.W))
+            {
+                inputMoveDirection.z = +1f;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                inputMoveDirection.z = -1f;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                inputMoveDirection.x = +1f;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                inputMoveDirection.x = -1f;
+            }
+
+            float moveSpeed = 5f;
+            Vector3 moveVector = transform.forward * inputMoveDirection.z + transform.right * inputMoveDirection.x;
+            transform.position += moveVector * (moveSpeed * Time.deltaTime);
+
+            Vector3 rotationVector = new Vector3(0, 0, 0);
+            if (Input.GetKey(KeyCode.Q))
+            {
+                rotationVector.y = +1f;
+            }
+
+            if (Input.GetKey(KeyCode.E))
+            {
+                rotationVector.y = -1f;
+            }
+
+            float rotationSpeed = 100f;
+            transform.eulerAngles += rotationVector * (rotationSpeed * Time.deltaTime);
+            
+            //Debug.Log(Input.mouseScrollDelta);
+            CinemachineTransposer cinemachineTransposer = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+            Vector3 followOffset = cinemachineTransposer.m_FollowOffset;
+            float zoomAmount = 1f;
+            if (Input.mouseScrollDelta.y > 0)
+            {
+                followOffset.y -= zoomAmount;
+            }
+            if (Input.mouseScrollDelta.y < 0)
+            {
+                followOffset.y += zoomAmount;
+            }
+            followOffset.y = Mathf.Clamp(followOffset.y, MIN_FOLLOW_Y_OFFSET, MAX_FOLLOW_Y_OFFSET);
+            cinemachineTransposer.m_FollowOffset = followOffset;
+        }
+    }
